452896d08e592d630fa45f944897b3f791ca09c5
kalun62 <80530886+kalun62@users.noreply.github.com>
2021-09-04 14:18:10 +0300

    Add files via upload

---
+++ Вербальный тест/main.js
@@ -0,0 +1,11 @@
+
+//     const button_var_a = document.getElementById('var_a');
+//     const button_var_b = document.getElementById('var_b');
+//     const table_a = document.getElementById('variant_a');
+//     const table_b = document.getElementById('variant_b');
+//     const desc = document.querySelector('.description')
+
+// jQuery(button_var_a).click(function(){
+// desc.replaceWith(table_a);
+// });
+
---
+++ Вербальный тест/redirect.js
@@ -0,0 +1,5 @@
+jQuery(document).ready(function(){
+    window.setTimeout(function(){
+        location.href = 'https://docs.google.com/forms/d/1jH6KYx_HmVjzt7LYUjxbWKpHnquoFy8iP8Dshc3fGug/edit';
+    },60000);
+})
